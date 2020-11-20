5386167f329f9bec431348107c13e70136420cd7
EvilBrain <buildtool@evilbrain.ml>
2020-11-20 22:36:19 +0100

    0.1.0-dev

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-var temptoken = require('../build/index');
+var temptoken = require('../src/index');
 
 var token = new temptoken.TempToken("1234");
-//console.log(token.make("saluttttt|&tempToken&|pedale", 18000000));
-console.log(token.checkToken("U2FsdGVkX1/ikga5qciiEOXrpJ7VxjA9T+YnfYPU2BhVHhaMipEHjo+3QOS3mILY+Ib/PPrumGm+w6jUl4RtphfKxgwtoB4kH+PKuP6Xm5k="))
+token.make("token test", 18000000);
+console.log(token.checkToken("U2FsdGVkX1/ikga5qciiEOXrpJ7VxjA9T+YnfYPU2BhVHhaMipEHjo+3QOS3mILY+Ib/PPrumGm+w6jUl4RtphfKxgwtoB4kH+PKuP6Xm5k="));
