bf3f104b18c5c870715cb994d4686d053873a660
EvilBrain <buildtool@evilbrain.ml>
2020-12-05 11:10:40 +0100

    0.2.1

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
-var temptoken = require('../src/index');
+var temptoken = require('temp-token');
 
 var token = new temptoken.TempToken("1234");
+
 token.make("token test", 18000000);
+
 console.log(token.checkToken("U2FsdGVkX1/ikga5qciiEOXrpJ7VxjA9T+YnfYPU2BhVHhaMipEHjo+3QOS3mILY+Ib/PPrumGm+w6jUl4RtphfKxgwtoB4kH+PKuP6Xm5k="));
